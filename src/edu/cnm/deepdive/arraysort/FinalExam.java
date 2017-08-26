5e13f54b9aee46246b303e476fabee5ad95926c5
kpescobar <kellyp.721@live.com>
2017-08-26 15:22:26 -0600

    Tested to make sure my file can be read.

--- src/edu/cnm/deepdive/arraysort/FinalExam.java
+++ src/edu/cnm/deepdive/arraysort/FinalExam.java
@@ -8,6 +8,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
@@ -17,36 +18,16 @@ import java.util.LinkedList;
 public class FinalExam {
 
   private static final String FINAL_BUNDLE = "resources/test-input.dat";
-  private static int[][] list = null;
+  
   
   /**
    * @param args
    */
   public static void main(String[] args) {
-    
-   
-
+    InputReader inputReader = new InputReader(FINAL_BUNDLE);
+    System.out.println(Arrays.toString(inputReader.list[0]));
   }
   
-  private static LinkedList<float[]> fileReader(String FINAL_BUNDLE) {
-    try (
-         FileInputStream stream = new FileInputStream(FINAL_BUNDLE);
-         InputStreamReader input = new InputStreamReader(stream);
-         BufferedReader reader = new BufferedReader(input); 
-    ) {
-      String line;
-      LinkedList<float[]> sorted = new LinkedList<>();
-      while ((line = reader.readLine()) != null) {
-        line = line.trim();
-        if (line.length() > 0) {
-          sorted.add(Integer.parseInt(line));
-        }
-      }
-      return results;
-    } catch (IOException ex) {
-      ex.printStackTrace();
-      throw new RuntimeException(ex);
-    }
-  }
+  
 
 }
--- src/edu/cnm/deepdive/arraysort/InputReader.java
+++ src/edu/cnm/deepdive/arraysort/InputReader.java
@@ -3,18 +3,45 @@
  */
 package edu.cnm.deepdive.arraysort;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
 /**
  * @author Kelly Escobar
  *
  */
 public class InputReader {
+  public static float[][] list;
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    InputReader(String fileToRead) {
+      File inputRead = new File(fileToRead);
+      try (
+          FileReader reader = new FileReader(fileToRead);
+          BufferedReader buffer = new BufferedReader(reader);
+      ) {
+        LinkedList<float[]> work = new LinkedList<>();
+        String array;
+        while ((array = buffer.readLine()) != null) {
+          if (array.trim().length() > 0) {
+            String[] values = array.trim().split("\\s+");
+            float[] floatVals = new float[values.length];
+            for (int i = 0; i < values.length; i++) {
+              floatVals[i] = Float.parseFloat(values[i]);
+            }
+            work.add(floatVals);
+          }
+        }
+        list = work.toArray(new float[0][]);
+      } catch (FileNotFoundException ex) {
+        ex.printStackTrace();
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    
   }
 
-}
